c633ac7661fa97af4025532eb292ec8827742a6e
Ayush-kush <ayushkushwah678@gmail.com>
2024-03-02 15:24:33 +0530

    Student class created

--- SchoolManagementSystem/src/school/management/system/Student.java
+++ SchoolManagementSystem/src/school/management/system/Student.java
@@ -2,10 +2,45 @@ package school.management.system;
 
 public class Student {
 
-    private int id;
-    private String name;
-    private int grade;
-    private int salary;
+    private int id;     //Unique Id for student
+    private String name;  // Name of the student
+    private int grade;  // Grade of the Student
+    private int feesPaid; // Amount of fees paid by the student
+    private int feesTotal; // Amount of fees due by the student
 
+    public Student(int id, String name, int grade){
+        this.feesPaid=0;
+        this.feesTotal=50000;
+        this.id=id;
+        this.name=name;
+        this.grade=grade;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getFeesPaid() {
+        return feesPaid;
+    }
+
+    public int getFeesTotal() {
+        return feesTotal;
+    }
+
+    public void updateFeesPaid(int payment){
+        feesPaid+=payment;
+    }
 }
